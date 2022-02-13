bd1bc2023424955135388d1d899811cb0272b1cb
Antoine Emond <antoine.emond.96@gmail.com>
2022-02-12 20:45:27 -0500

    get the data

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,11 @@ import fs from "fs";
 
 const app = express();
 const port = 3000;
-let data = fs.readFileSync("./data/limadmin.json");
+let rawData = JSON.parse(fs.readFileSync("./data/limadmin.json", "utf-8"));
 
 app.get("/", (req, res) => {
-  console.log("hello est-ce que tu vois ");
-
-  res.send({ data });
+  console.log(rawData);
+  res.send({ rawData });
 });
 
 app.listen(port, () => {
