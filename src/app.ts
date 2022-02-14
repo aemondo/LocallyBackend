0f48c94953bbcfc20aa97d9f5f80bef2e011be4f
Antoine Emond <antoine.emond.96@gmail.com>
2022-02-14 10:07:08 -0500

    needs weuryParams

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,37 @@
 import express from "express";
 import fs from "fs";
+import * as turf from "@turf/turf";
+import { MultiPolygon, Polygon } from "@turf/turf";
+
+interface GeoPoint {
+  lat: number;
+  lng: number;
+}
+
+interface Region {
+  name: string;
+  shape: number[][][];
+}
 
 const app = express();
 const port = 3000;
 let rawData = JSON.parse(fs.readFileSync("./data/limadmin.json", "utf-8"));
 
+const regions: Region[] = rawData.features.map((feature) => ({
+  name: feature.properties.NOM,
+  shape: feature.geometry.coordinates[0],
+}));
+
 app.get("/", (req, res) => {
-  console.log(rawData);
-  res.send({ rawData });
+  console.table(regions);
+
+  const myLocation = turf.point([-73.634444, 45.432114]);
+
+  const foundRegion = regions.find((region) =>
+    turf.booleanPointInPolygon(myLocation, turf.polygon(region.shape))
+  );
+
+  res.send({ foundRegion });
 });
 
 app.listen(port, () => {
